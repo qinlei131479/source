d48f452e8d576bc368fe0ddb9455c9920c016711
qinlei <hedan131479@163.com>
2021-06-27 14:12:49 +0800

    全局唯一key优化

--- course-learn/course-springboot/src/main/java/com/course/springboot/service/impl/KeyServiceImpl.java
+++ course-learn/course-springboot/src/main/java/com/course/springboot/service/impl/KeyServiceImpl.java
@@ -2,6 +2,7 @@ package com.course.springboot.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.course.common.component.RedisUtil;
 import com.course.common.enums.FlagEnum;
 import com.course.mybatis.service.impl.UpServiceImpl;
 import com.course.springboot.entity.Key;
@@ -11,12 +12,16 @@ import com.course.springboot.service.KeyService;
 import lombok.RequiredArgsConstructor;
 
 /**
+ * 全局唯一key生成
+ * 
  * @author qinlei
  * @date 2021/6/25 下午5:47
  */
 @Service
 @RequiredArgsConstructor
 public class KeyServiceImpl extends UpServiceImpl<KeyMapper, Key> implements KeyService {
+
+	private final RedisUtil redisUtil;
 
 	@Override
 	public Long updateKey() {
@@ -28,11 +33,11 @@ public class KeyServiceImpl extends UpServiceImpl<KeyMapper, Key> implements Key
 	@Override
 	public Long prepare(Key req) {
 		if (req != null && FlagEnum.checkYes(req.getEnableFlag())) {
-			Long size = 0L;// this.redisUtil.keySize();
+			Long size = this.redisUtil.keySize();
 			if (size < req.getMin()) {
 				Long createNum = req.getMax() - size;
 				for (int i = 0; i < createNum; i++) {
-					// this.redisUtil.keyRightPush(this.updateKey());
+					this.redisUtil.keyRightPush(this.updateKey());
 				}
 				return createNum;
 			} else {

a2481e1504614691af887c6e95b8a0443a888a19
qinlei <hedan131479@163.com>
2021-07-07 15:10:52 +0800

    course-learn子项目更名

---
+++ course-learn/course-learn-mybatisplus/src/main/java/com/course/mybatis/plus/entity/Role.java
@@ -0,0 +1,98 @@
+package com.course.mybatis.plus.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 角色信息表
+ * </p>
+ *
+ * @author qinlei
+ * @since 2021-06-02
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("sys_role")
+public class Role implements Serializable {
+
+private static final long serialVersionUID=1L;
+
+    /**
+     * 角色ID
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 角色名称
+     */
+    private String name;
+
+    /**
+     * 角色权限字符串
+     */
+    @TableField("roleKey")
+    private String roleKey;
+
+    /**
+     * 显示顺序
+     */
+    private Integer weight;
+
+    /**
+     * 数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限）
+     */
+    @TableField("dataScope")
+    private Integer dataScope;
+
+    /**
+     * 角色状态（0正常 1停用）
+     */
+    private Integer status;
+
+    /**
+     * 删除标志（0代表存在 1代表删除）
+     */
+    @TableField("isDelete")
+    private Integer isDelete;
+
+    /**
+     * 创建者
+     */
+    @TableField("createBy")
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    @TableField("createTime")
+    private LocalDateTime createTime;
+
+    /**
+     * 更新者
+     */
+    @TableField("updateBy")
+    private String updateBy;
+
+    /**
+     * 更新时间
+     */
+    @TableField("updateTime")
+    private LocalDateTime updateTime;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+
+}
---
+++ course-learn/course-learn-mybatisplus/src/main/java/com/course/mybatis/plus/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.course.mybatis.plus.mapper;
+
+import com.course.mybatis.plus.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户信息表 Mapper 接口
+ * </p>
+ *
+ * @author qinlei
+ * @since 2021-06-02
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
